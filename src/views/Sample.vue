75be7536cb1ebe6b5b45a9ce89570c9d1268dbe5
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-31 20:44:21 -0400

    add boilerplate to new sample page

---
+++ src/views/Sample.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="sample">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Welcome to the new sample Vue.js page!",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
